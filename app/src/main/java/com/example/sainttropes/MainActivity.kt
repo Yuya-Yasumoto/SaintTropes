56c921d109c2965839bdb4a48e0d1c5492aed4fe
yuyayasumoto <yuya.yasumoto@jp.ricoh.com>
2019-09-30 10:06:24 +0900

    こみっとー

--- app/src/main/java/com/example/sainttropes/MainActivity.kt
+++ app/src/main/java/com/example/sainttropes/MainActivity.kt
@@ -2,6 +2,10 @@ package com.example.sainttropes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ContextMenu
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,7 +14,60 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //HTML表示の最低限の設定
         webView.settings.javaScriptEnabled = true
         webView.loadUrl("file:///android_asset/html/index.html")
+
+        registerForContextMenu(webView)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when(item?.itemId){
+            R.id.top -> {
+                webView.loadUrl("file:///android_asset/html/index.html")
+                return true
+            }
+            R.id.lunch01 -> {
+                webView.loadUrl("file:///android_asset/html/lunch01.html")
+                return true
+            }
+            R.id.lunch02 -> {
+                webView.loadUrl("file:///android_asset/html/lunch02.html")
+                return true
+            }
+            R.id.dinner01 -> {
+                webView.loadUrl("file:///android_asset/html/dinner01.html")
+                return true
+            }
+            R.id.dinner02 -> {
+                webView.loadUrl("file:///android_asset/html/dinner02.html")
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onCreateContextMenu(
+        menu: ContextMenu?,
+        v: View?,
+        menuInfo: ContextMenu.ContextMenuInfo?
+    ) {
+        super.onCreateContextMenu(menu, v, menuInfo)
+        menuInflater.inflate(R.menu.context,menu)
+    }
+
+    override fun onContextItemSelected(item: MenuItem?): Boolean {
+        when(item?.itemId){
+            R.id.sms -> return true
+            R.id.mail -> return true
+            R.id.share -> return true
+            R.id.brouse -> return true
+        }
+        return super.onContextItemSelected(item)
     }
 }
